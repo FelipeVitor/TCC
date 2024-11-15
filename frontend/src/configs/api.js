4586658415cd6662b1146a44da1b88741762b9e0
felipe <felipesv1900@hotmail.com>
2024-11-15 15:17:06 -0300

    Update API URL configuration to support environment variable fallback

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
-
-const REACT_APP_API_URL = "http://localhost:9000"
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://localhost:9000';
 
 console.log('REACT_APP_API_URL', REACT_APP_API_URL);
 
